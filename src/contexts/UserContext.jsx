9843d7802d2f2f28873e8e0b86e6caf05be8c12c
erick-bonfati <erick.bonfati@policorp.com.br>
2025-10-27 16:11:56 -0300

    Mantendo login de usuario durante 7 dias

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,16 +1,34 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 
 export const UserContext = createContext()
 
 export default function UserProvider({ children }) {
   const [usuario, setUsuario] = useState(null)
 
+  // Condição para armazenar login de usuário no localStorage por 7 dias
+  useEffect(() => {
+    const usuarioSalvo = localStorage.getItem("usuario")
+    if(usuarioSalvo) {
+      const {nome, dataExpira } = JSON.parse(usuarioSalvo)
+      if(Date.now() < dataExpira) {
+        setUsuario(nome)
+      } else {
+        localStorage.removeItem("usuario")
+      }
+    }
+  })
+
   const login = (nome) => {
     setUsuario({ nome })
+    localStorage.setItem("usuario", JSON.stringify({
+      nome,
+      dataExpira: Date.now() + 7 * 24 * 60 * 60 * 1000 // 7 DIAS
+    }))
   }
 
   const logout = () => {
     setUsuario(null)
+    localStorage.removeItem("usuario")
   }
 
   return (
